c96d3ad6f0c3f26cbbe57ae72c62fb2d0cf95e22
Nícolas Lopes <nicolasinott@gmail.com>
2022-09-30 22:53:21 +0200

    Code for the Allocator constructor

--- DM/src/Allocator.java
+++ DM/src/Allocator.java
@@ -8,6 +8,34 @@ public class Allocator {
 
 	Allocator(int size) {
 		// TODO
+
+		this.maxk = size;
+
+		for(int i = 0; i<size; i++){
+			HashSet<Integer> set_of_addr = new HashSet<Integer>();
+			this.freeblocks.add(set_of_addr);
+		}
+
+		HashSet<Integer> set_of_addr = new HashSet<Integer>();
+		set_of_addr.add(0);
+
+		this.freeblocks.add(set_of_addr);
+
+		//while(size != 1){
+		//	HashSet<Integer> set_of_addr = new HashSet<Integer>();
+		//	int number_of_addr = this.size / size;
+		//	
+		//	for(int i = 0; i < number_of_addr ; i++){
+		//		set_of_addr.add((Integer)(i*size));
+		//	}
+
+		//	this.freeblocks.add(set_of_addr);
+
+		//	size-- ;
+		//}
+
+		// TODO
+
 		// Écrire le constructeur n'est pas une question de l'énoncé,
 		// mais il faut le faire pour tester votre code
 	}
@@ -23,8 +51,20 @@ public class Allocator {
 	}
 
 	int reserve(int k) {
+
+		Integer addr = this.freeblocks.elementAt(k).iterator().next();
+
+		this.freeblocks.get(k);
+
+		if(addr != null){
+			this.freeblocks.elementAt(k).remove(addr);
+			return addr;
+		} else {
+			throw new RuntimeException();
+		}
+
 		// TODO
-		return 0;
+		//return 0;
 	}
 
 	public int alloc(int size) {
@@ -34,5 +74,10 @@ public class Allocator {
 
 	public void free(int addr, int size) {
 		// TODO
+	}
+
+	public static void main(String[] args) {
+		Allocator a = new Allocator(4);
+		System.out.println(a.freeblocks.get(4));
 	}
 }
